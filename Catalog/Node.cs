5e2d88874a5a831743a1d0c560399da3df2ecedf
dmlittledm <dmlittle@pochta.ru>
2015-01-31 03:24:16 +0300

    Start

---
+++ Catalog/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog
+{
+    /// <summary> Catalogue node 
+    /// </summary>
+    public class Node
+    {
+        protected Guid Id { get; set; }
+        
+        public string Name;
+        public string Description;
+        public Image Image;
+
+        public string Path;
+
+        public bool HasChilds {get { return Childs.Any(); }}
+        public bool HasLinks {get { return Links.Any(); }}
+
+        public List<Node> Childs { get; private set; }
+        /// <summary> Links to other resources 
+        /// </summary>
+        public List<Link> Links { get; private set; }
+
+        /// <summary> parent node 
+        /// </summary>
+        public Node Parent { get; private set; }
+
+
+
+    }
+
+    public class Link
+    {
+        
+    }
+}
