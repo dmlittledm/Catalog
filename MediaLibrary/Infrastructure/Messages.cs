1bce18c437e982ea1f1a00fa0e7e8de20a319f8c
dmlittledm <dmlittle@yandex.ru>
2017-05-12 04:54:35 +0300

    Resource controller. Some changes.

---
+++ MediaLibrary/Infrastructure/Messages.cs
@@ -0,0 +1,18 @@
+﻿namespace MediaLibrary.Infrastructure
+{
+    /// <summary> Class containing application messages 
+    /// </summary>
+    internal static class Messages
+    {
+        public static class Resource
+        {
+            public const string AlreadyContainsFieldWithNameXxx = "Ресурс уже содержит поле с именем {0}.";
+        }
+
+        public static class Directory
+        {
+            public const string AlreadyContainsResourceWithIdXxx = "Справочник уже содержит ресурс с Id {0}.";
+            public const string AlreadyContainsFieldTypeWithNameXxx = "Справочник уже содержит поле с именем {0}.";
+        }
+    }
+}
